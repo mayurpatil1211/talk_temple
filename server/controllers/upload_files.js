d67154acf73c27317297760cb897fa1ac99a8337
Mayur Patil <mayur.patil1211@gmail.com>
2018-04-19 11:37:08 +0530

    swd

--- server/controllers/upload_files.js
+++ server/controllers/upload_files.js
@@ -22,7 +22,7 @@ var storage = multer.diskStorage({
   	}else{
   		var assigned_filename = Date.now()+ '_' +file.originalname
   			Image.create({title: req.body.title, image:'/images/'+String(assigned_filename)})
-  			.then(image=>next())
+  			.then(image=>cb())
   			.catch(err=>cb(err))
   		cb(null, assigned_filename)
   	}
